228dd7144a7aec29ad711631269355aed117c60b
longtaosu <859122649@qq.com>
2020-03-01 09:58:17 +0800

    初始化

---
+++ 01Initialize/src/components/HelloVue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="CopClass">
+      <h1>{{msg}}</h1>
+      <h2>Element UI Button</h2>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="text">文字按钮</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloVue extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
